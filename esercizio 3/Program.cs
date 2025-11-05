b50946282a38593d7be9b478c68882e83e609c72
T1mp4ve1 <pavelwire@outlook.it>
2025-11-05 21:52:29 +0100

    esercizio 3

---
+++ esercizio 3/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        int somm = 0;
+        int arrayLen;
+        Console.WriteLine("Di quanti elementi voi creare l'array?");
+        arrayLen = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < arrayLen; i++)
+        {
+            Console.WriteLine($"Devi inserire {arrayLen - i} numeri");
+            Console.WriteLine("Inserisci il numero: ");
+            somm += int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine($"La somma: {somm}");
+        Console.WriteLine($"La media: {somm / arrayLen} ");
+    }
+}
