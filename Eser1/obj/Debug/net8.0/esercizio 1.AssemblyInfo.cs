4539308b8b8860e87d7660af542156c10aad93dc
T1mp4ve1 <pavelwire@outlook.it>
2025-11-05 20:12:06 +0100

    rinomina

--- Eser1/obj/Debug/net8.0/esercizio 1.AssemblyInfo.cs
+++ Eser1/obj/Debug/net8.0/esercizio 1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Eser1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("esercizio 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99affff041c5dc4c54f21f826ded3077c1682f26")]
-[assembly: System.Reflection.AssemblyProductAttribute("Eser1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Eser1")]
+[assembly: System.Reflection.AssemblyProductAttribute("esercizio 1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("esercizio 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generato dalla classe WriteCodeFragment di MSBuild.
